97abf25de2ed1d6550f051d80ed6bd7c19384f76
iiM1chexl <82762387+iiM1chexl@users.noreply.github.com>
2021-06-04 01:54:00 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+console.log("Hi M1ch")
+//====================================================================================CONSTANTS REQUIRED ON READY=============================================================================================
+const { Client, Collection } = require('discord.js');
+const { PREFIX, TOKEN } = require('./config');
+const bot = new Client({ disableMentions: 'everyone' });
+const fs = require("fs");
+const request = require("request");
+const axios = require("axios");
+const snekfetch = require("snekfetch");
+const fetch = require("node-fetch");
+const enmap = require('enmap');
+const db = require('quick.db');
+const keepAlive = require("./server");
+const mySecret = process.env['TOKEN'];
+
+
+
+
+//============================================================================================================================================================================================================
+const settings = new enmap({
+    name: "settings",
+    autoFetch: true,
+    cloneLevel: "deep",
+    fetchAll: true
+});
+
+
+//====================================================================================COLLECTIONS REQUIRED ON READY===========================================================================================
+
+bot.commands = new Collection();
+bot.aliases = new Collection();
+
+//============================================================================================================================================================================================================
+
+
+
+//============================================================================================INITIALIZING====================================================================================================
+["aliases", "commands"].forEach(x => bot[x] = new Collection());
+["console", "command", "event"].forEach(x => require(`./handler/${x}`)(bot));
+
+bot.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+    require(`./handler/${handler}`)(bot);
+});
+
+//============================================================================================================================================================================================================
+
+
+//=========================================================================================MENTION SETTINGS===========================================================================================
+
+bot.on('message', async message => {
+
+
+    let prefix;
+        try {
+            let fetched = await db.fetch(`prefix_${message.guild.id}`);
+            if (fetched == null) {
+                prefix = PREFIX
+            } else {
+                prefix = fetched
+            }
+        
+            } catch {
+            prefix = PREFIX
+    };
+    try {
+        if (message.mentions.has(bot.user.id) && !message.content.includes("@everyone") && !message.content.includes("@here")) {
+          message.channel.send(`\n<@${message.author.id}> My prefix for\`${message.guild.name}\` is \`${prefix}\``);
+          }
+          
+    } catch {
+        return;
+    };
+    
+
+
+});
+
+
+//============================================================================================================================================================================================================
+
+keepAlive()
+bot.login(mySecret);
